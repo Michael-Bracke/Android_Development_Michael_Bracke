c17ddf93f2e227296ca8d7f58ef3b59ef286b79f
Michaël Bracké <michael.bracke@becosoft.be>
2021-12-30 21:28:38 +0100

    Music Service : Threading voorzien + Extensie om te kunnen controleren of deze reeds draaid ofniet

---
+++ app/src/main/java/secret/santa/application/extensions/ClassExtensions.kt
@@ -0,0 +1,10 @@
+package secret.santa.application.extensions
+
+import android.app.ActivityManager
+import android.content.Context
+
+fun Context.isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        return manager.getRunningServices(Integer.MAX_VALUE)
+            .any { it.service.className == serviceClass.name }
+}
--- app/src/main/java/secret/santa/application/services/MusicServiceSSA.java
+++ app/src/main/java/secret/santa/application/services/MusicServiceSSA.java
@@ -2,11 +2,17 @@ package secret.santa.application.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
+import android.provider.MediaStore;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 
 import com.secret.santa.R;
+
 
 public class MusicServiceSSA extends Service {
 
@@ -21,6 +27,22 @@ public class MusicServiceSSA extends Service {
     public void onCreate(){
         ssaMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.christmas_background);
         ssaMediaPlayer.setLooping(false);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // basically maak je hier een nieuwe thread aan die adhv de runnable()
+        // een thread zal opstarten wanneer deze wordt opgestart
+        new Thread(
+                new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Service", "Musicservice is gestart..");
+                ssaMediaPlayer.start();
+                Log.e("Service", "Is playing? " + ssaMediaPlayer.isPlaying());
+            }
+        }).start();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     public void onStart(Intent intent, int startid){
