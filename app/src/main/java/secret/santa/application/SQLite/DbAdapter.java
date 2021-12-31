e90142fc29bcca40f4cce1e1e03ac35300224f85
Michaël Bracké <michael.bracke@becosoft.be>
2021-12-31 15:00:44 +0100

    SQL persistentie - Adapter aanpassen analogie USER dat reeds gestored wordt in FB

--- app/src/main/java/secret/santa/application/SQLite/DbAdapter.java
+++ app/src/main/java/secret/santa/application/SQLite/DbAdapter.java
@@ -1,6 +1,7 @@
 package secret.santa.application.SQLite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -12,16 +13,32 @@ public class DbAdapter {
         helper = new myDbHelper(context);
     }
 
+    public String getData()
+    {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] columns = {myDbHelper.UID,myDbHelper.NAME,myDbHelper.PROFILEIMGURL};
+        Cursor cursor =db.query(myDbHelper.TABLE_NAME,columns,null,null,null,null,null);
+        StringBuffer buffer= new StringBuffer();
+        while (cursor.moveToNext())
+        {
+            int cid =cursor.getInt(cursor.getColumnIndex(myDbHelper.UID));
+            String name =cursor.getString(cursor.getColumnIndex(myDbHelper.NAME));
+            String  password =cursor.getString(cursor.getColumnIndex(myDbHelper.PROFILEIMGURL));
+            buffer.append(cid+ "   " + name + "   " + password +" \n");
+        }
+        return buffer.toString();
+    }
+
     static class myDbHelper extends SQLiteOpenHelper
     {
         private static final String DATABASE_NAME = "myDatabase";    // Database Name
         private static final String TABLE_NAME = "myTable";   // Table Name
         private static final int DATABASE_Version = 1;    // Database Version
-        private static final String UID="_id";     // Column I (Primary Key)
+        private static final String UID="Uid";     // Column I (Primary Key)
         private static final String NAME = "Name";    //Column II
-        private static final String MyPASSWORD= "Password";    // Column III
+        private static final String PROFILEIMGURL= "ProfileImageUrl";    // Column III
         private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+
-                " ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255) ,"+ MyPASSWORD+" VARCHAR(225));";
+                " ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255) ,"+ PROFILEIMGURL+" VARCHAR(225));";
         private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
         private Context context;
 
