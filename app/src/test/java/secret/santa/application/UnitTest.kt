f492b59a90acb4b5627059252c7bc2fd8eccb264
Michaël Bracké <michael.bracke@becosoft.be>
2022-01-01 15:09:31 +0100

    Junit test - Testen voorzien authservice

--- app/src/test/java/secret/santa/application/UnitTest.kt
+++ app/src/test/java/secret/santa/application/UnitTest.kt
@@ -1,8 +1,13 @@
 package com.secret.santa
 
-import org.junit.Test
 
+
+import android.content.res.Resources
+import org.junit.Test
+import com.parse.Parse
 import org.junit.Assert.*
+import secret.santa.application.services.AuthService
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +15,42 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class UnitTest {
+
+    val mAuth : AuthService = AuthService()
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun emptyUsernameReturnsError() {
+
+        val inptName = "";
+        val inptEmail = "test25@live.be";
+        val inptPass = "whatisthislogic12345";
+
+        mAuth.Register(inptName,inptEmail, inptPass) {
+            returnVal -> assertEquals(returnVal, "Please check your name")
+        }
+    }
+
+    @Test
+    fun emptyEmailReturnsError() {
+
+
+        val inptName = "qzdqzdqzdqd";
+        val inptEmail = "";
+        val inptPass = "whatisthislogic12345";
+
+        mAuth.Register(inptName,inptEmail, inptPass) {
+                returnVal -> assertEquals(returnVal, "Please check your email")
+        }
+    }
+
+    @Test
+    fun emptyPassReturnsError() {
+
+        val inptName = "qzdqzddqzdzad";
+        val inptEmail = "test25@live.be";
+        val inptPass = "";
+
+        mAuth.Register(inptName,inptEmail, inptPass) {
+                returnVal -> assertEquals(returnVal, "Please check your password")
+        }
     }
 }
